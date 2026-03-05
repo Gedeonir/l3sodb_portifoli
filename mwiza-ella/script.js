32784cdfcc2dd53ed78624abe12be1733ccddc68
Ella blessing <mwizaellablessing29@gmail.com>
2026-03-05 04:01:16 -0800

    added card

--- mwiza-ella/script.js
+++
@@ -1,7 +0,0 @@
-// Welcome message in console
-console.log("Welcome to Ella's Portfolio Website!");
-
-// Button interaction
-function sayHello() {
-    alert("Hello! Welcome to Ella's Web Developer Portfolio 👩🏽‍💻");
-}
